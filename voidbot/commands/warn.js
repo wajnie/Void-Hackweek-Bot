67926d11d414dd54ad6077f04e7b328b10957ef9
Michał Waligóra <49998336+wajnie@users.noreply.github.com>
2019-06-28 13:43:41 +0200

    Update warn.js

--- voidbot/commands/warn.js
+++ voidbot/commands/warn.js
@@ -38,28 +38,28 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.send("**Bots can't be warned, silly**")
     let reason = args.join(" ").slice(22);
 
-    if (!warns[wUser.id]) warns[wUser.id] = {
+    if (!warns[warnUser.id]) warns[warnUser.id] = {
         warns: 0
     };
 
-    warns[wUser.id].warns++;
+    warns[warnUser.id].warns++;
 
     fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
         if (err) console.log(err)
     });
 
     message.channel.send(`**${user.tag}** has been warned.`).catch(console.error)
-    if (warns[wUser.id].warns == 3) {
+    if (warns[warnUser.id].warns == 3) {
         if (!memberr.kickable)
             return message.channel.send(`I can't kick **${use.tag}** because of his \`3\` warns. Maybe he is a higher rank than me, or i don't have enough permissions.`);
         message.channel.send(`**${user.tag}** has been kicked for having \`3\` warns`)
-        message.guild.member(wUser).kick("3 warns")
+        message.guild.member(warnUser).kick("3 warns")
     }
-    if (warns[wUser.id].warns == 6) {
+    if (warns[warnUser.id].warns == 6) {
         if (!memberr.bannable)
             return message.channel.send(`I can't ban ${use.tag}** because of his \`6\` warns. Maybe he is a higher rank than me, or i don't have enough permissions.`);
         message.channel.send(`**${user.tag}** has been banned for having \`6\` warns!`)
-        message.guild.member(wUser).ban("6 warns")
+        message.guild.member(warnUser).ban("6 warns")
     }
 }
 module.exports.help = {

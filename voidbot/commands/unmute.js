bf14eeb60eb0e23948f596fbddb03e6b4e2defef
Michał Waligóra <49998336+wajnie@users.noreply.github.com>
2019-06-28 13:40:49 +0200

    Update unmute.js

--- voidbot/commands/unmute.js
+++ voidbot/commands/unmute.js
@@ -13,24 +13,24 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.send(aakickembedd)
     }
     let mute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    // let prefix = prefixes[message.guild.id].prefixes;
+
     const emved = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setColor('FF0000')
         .setDescription(`Missing \`MANAGE_ROLES\` permission\n\nUser doesn't have Manage Roles permission.`)
-        
+
     if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(emved)
     const ended = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setColor('FF0000')
         .setDescription(`Missing \`[user]\` argument\nCorrect usage:\n\`unmute | [user]`)
-        
+
     if (!args[0]) return message.channel.send(ended)
 
     if (message.mentions.users.size === 0) {
         return message.reply(ended).catch(console.error);
     }
-    
+
     if (!mute.roles.find("name", "muted")) {
         const embedd = new Discord.RichEmbed()
             .setAuthor(message.author.tag, message.author.avatarURL)
@@ -38,7 +38,7 @@ module.exports.run = async (bot, message, args) => {
             .setDescription(`User is not muted.`)
         return message.channel.send(embedd)
     }
-    
+
     const embed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setColor('7FFF00')
